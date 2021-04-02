c51d705583f1105bd925339951acaacd266fe43d
“darcy504” <Darcyjdraper@gmail.com>
2021-04-02 18:53:32 -0500

    Move Left Function

---
+++ script.js
@@ -0,0 +1,7 @@
+var character = document.getElementById("character");
+
+function moveLeft() {
+    var left = 
+    parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    character.style.left = left + 2 + "px";
+}
