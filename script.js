bb4d031fd9243211f4dbd66f901b8a5eb6b6af2c
“darcy504” <Darcyjdraper@gmail.com>
2021-04-02 18:55:53 -0500

    Move Right Function

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function moveLeft() {
     parseInt(window.getComputedStyle(character).getPropertyValue("left"));
     character.style.left = left + 2 + "px";
 }
+
+function moveRight() {
+    var left = 
+    parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    character.style.left = left - 2 + "px";
+}
